bac07b05a5cc64e00c40cbb50a9651bac9c8ad4b
Eduardo <ewang@bionexo.com>
2022-08-29 15:27:20 -0300

    Starting the project

---
+++ web_discovery.py
@@ -0,0 +1,16 @@
+import requests
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+targetURL = input("Enter Target URL: ")
+file = open("common.txt", "r")
+for line in file:
+    line = line.strip('\n')
+    fullURL = targetURL + "/" + line
+    response = request(fullURL)
+    if response:
+        print('[+] Discovered Directory at Link: ' + fullURL)
